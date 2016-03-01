ca68cf0d400e683ec0559395245ef04ce7d56c74
Matthew Ames <git@matty.digital>
2016-03-01 18:27:26 +0000

    removed gallery stylesheet

--- functions.php
+++ functions.php
@@ -8,4 +8,10 @@ function register_my_menus() {
 }
 
 add_action( 'init', 'register_my_menus')
+
+function tweakjp_rm_tiledcss(){
+    wp_dequeue_style( 'tiled-gallery' );
+}
+add_action( 'wp_footer', 'tweakjp_rm_tiledcss' );
+
 ?>
