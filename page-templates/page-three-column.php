f5ef7ada69919ca3da59b7de97eb5a4a29c477fd
Matthew Ames <git@matty.digital>
2016-03-02 11:09:28 +0000

    added three column template

--- page-templates/page-three-column.php
+++ page-templates/page-three-column.php
@@ -1,1 +1,29 @@
 <?php /* Template Name: Three Column */ ?>
+
+<?php
+    wp_head();
+    get_header();
+?>
+      <div class="row main">
+      <div class="col-sm4">
+        <?php the_field('col1'); ?>
+      </div>
+      <div class="col-sm4">
+        <?php the_field('col1'); ?>
+      </div>
+      <div class="col-sm4">
+        <?php the_field('col1'); ?>
+      </div>
+<?php
+    if ( have_posts() ) : while ( have_posts() ) : the_post();
+        the_content();
+    endwhile;
+    else:
+        _e('Sorry, no posts matched your criteria.');
+    endif;
+?>
+      </div>
+<?php
+    get_footer();
+    wp_footer();
+?>
