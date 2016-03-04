3dd95414cfd38f7cfc9313b9f485ebc53b8fddb8
Matthew Ames <matthew@tim.home>
2016-03-04 21:20:15 +0000

    body height 100%

---
+++ js/icarousel.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+    imglist = []
+
+    $(".imggrid").find("a.overlay").each(function() {
+        url = $(this).attr("href");
+        if (url != "undefined") {
+            imglist.push(url);
+        }
+
+    });
+
+    numitems = imglist.length;
+
+    $("body").append('<div id="carouselbg" style="position: fixed; top: 0; left: 0px; bottom: 0px; right: 0px; background-color: #FFF;"></div>')
+
+    $("#carouselbg").append('<div id="imgcarousel" class="carousel slide" data-ride="carousel" style="position: fixed; top: 10%, bottom: 10%; left: 10%; right: 10%" ></div>');
+    $("#imgcarousel").append('<ol class="carousel-indicators"></ol>')
+
+    $("#imgcarousel").append('<div class="carousel-inner" role="listbox">')
+
+    for (i = 0; i < numitems; i++) {
+        active = ""
+        if (i == 0) {
+            active = "active"
+        }
+        $(".carousel-indicators").append('<li data-target="#imgcarousel" data-slide-to="' + i + '" class="' + active + '"></li>')
+
+        $(".carousel-inner").append('<div class="' + active + '" role="listbox"')
+        $(".carousel-inner").append('<center><img src="' + imglist[i] + '" style="max-width: 70%; max-height: 7%"></center>')
+        $(".carousel-inner").append('</div>')
+    }
+
+    $("#imgcarousel").append('</div>')
+
+    $("#imgcarousel").append('<a class="left carousel-control" href="#imgcarousel" role="button" data-slide="prev">')
+    $("#imgcarousel").append('<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>')
+    $("#imgcarousel").append('<span class="sr-only">Previous</span>')
+    $("#imgcarousel").append('</a>')
+    $("#imgcarousel").append('<a class="right carousel-control" href="#imgcarousel" role="button" data-slide="next">')
+    $("#imgcarousel").append('<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>')
+    $("#imgcarousel").append('<span class="sr-only">Next</span>')
+    $("#imgcarousel").append('</a>')
+
+
+    $(".imggrid").find("a").each(function() {
+        $(this).click(function(event) {
+            event.preventDefault();
+            alert($(this).attr("href"))
+        });
+    });
+});
