31777d0abad8857d9b6a2c776b83a121c1d7b290
Matthew Ames <matthew@tim.home>
2016-03-04 21:29:00 +0000

    body height 100%

--- js/icarousel.js
+++ js/icarousel.js
@@ -12,9 +12,9 @@ $(document).ready(function() {
 
     numitems = imglist.length;
 
-    $("body").append('<div id="carouselbg" style="position: fixed; top: 0; left: 0px; bottom: 0px; right: 0px; background-color: #FFF;"></div>')
+    $("body").append('<div id="carouselbg" style="position: fixed; top: 0px; left: 0px; bottom: 0px; right: 0px; background-color: #FFF;"></div>')
 
-    $("#carouselbg").append('<div id="imgcarousel" class="carousel slide" data-ride="carousel" style="position: fixed; top: 10%, bottom: 10%; left: 10%; right: 10%" ></div>');
+    $("#carouselbg").append('<div id="imgcarousel" class="carousel slide" data-ride="carousel"></div>');
     $("#imgcarousel").append('<ol class="carousel-indicators"></ol>')
 
     $("#imgcarousel").append('<div class="carousel-inner" role="listbox">')
@@ -27,7 +27,7 @@ $(document).ready(function() {
         $(".carousel-indicators").append('<li data-target="#imgcarousel" data-slide-to="' + i + '" class="' + active + '"></li>')
 
         $(".carousel-inner").append('<div class="' + active + '" role="listbox"')
-        $(".carousel-inner").append('<center><img src="' + imglist[i] + '" style="max-width: 70%; max-height: 7%"></center>')
+        $(".carousel-inner").append('<center><img src="' + imglist[i] + '" style="max-width: 60%; max-height: 60%"></center>')
         $(".carousel-inner").append('</div>')
     }
 
