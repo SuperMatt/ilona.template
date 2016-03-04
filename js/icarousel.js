a4d2f5200330fb40d99ff3da8a83a3f06f3b0e02
Matthew Ames <matthew@tim.home>
2016-03-04 22:42:43 +0000

    added glyphicons

--- js/icarousel.js
+++ js/icarousel.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     imglist = []
 
-    $(".imggrid").find("a.overlay").each(function() {
+    $("body").find("a.overlay").each(function() {
         url = $(this).attr("href");
         if (url != "undefined") {
             imglist.push(url);
@@ -13,40 +13,46 @@ $(document).ready(function() {
     numitems = imglist.length;
 
     $("body").append('<div id="carouselbg" style="position: fixed; top: 0px; left: 0px; bottom: 0px; right: 0px; background-color: #FFF;"></div>')
+    $("#carouselbg").hide();
 
     $("#carouselbg").append('<div id="imgcarousel" class="carousel slide" data-ride="carousel"></div>');
-    $("#imgcarousel").append('<ol class="carousel-indicators"></ol>')
 
-    $("#imgcarousel").append('<div class="carousel-inner" role="listbox">')
+    $("#imgcarousel").append('<div class="carousel-inner" role="listbox"></div>')
 
     for (i = 0; i < numitems; i++) {
         active = ""
         if (i == 0) {
             active = "active"
         }
-        $(".carousel-indicators").append('<li data-target="#imgcarousel" data-slide-to="' + i + '" class="' + active + '"></li>')
 
-        $(".carousel-inner").append('<div class="' + active + '" role="listbox"')
-        $(".carousel-inner").append('<center><img src="' + imglist[i] + '" style="max-width: 60%; max-height: 60%"></center>')
-        $(".carousel-inner").append('</div>')
+        $(".carousel-inner").append('<div id="item' + i + '" class="item ' + active + '"></div>')
+        $("#item" + i).append('<center><img style="max-width: 970px; height: 100%;" src="' + imglist[i] + '" class="" ></center>')
     }
 
-    $("#imgcarousel").append('</div>')
+    $("#imgcarousel").append('<a class="left carousel-control" style="background-image: none;" href="#imgcarousel" role="button" data-slide="prev"></a>')
+    $(".left").append('<span style="color: #333" class="glyphicon glyphicon-chevron-left" aria-hidden="false"></span>')
+    $(".left").append('<span class="sr-only">Previous</span>')
+    $("#imgcarousel").append('<a class="right carousel-control" style="background-image: none;" href="#imgcarousel" role="button" data-slide="next"><a>')
+    $(".right").append('<span style="color: #333" class="glyphicon glyphicon-chevron-right" aria-hidden="false"></span>')
+    $(".right").append('<span class="sr-only">Next</span>')
 
-    $("#imgcarousel").append('<a class="left carousel-control" href="#imgcarousel" role="button" data-slide="prev">')
-    $("#imgcarousel").append('<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>')
-    $("#imgcarousel").append('<span class="sr-only">Previous</span>')
-    $("#imgcarousel").append('</a>')
-    $("#imgcarousel").append('<a class="right carousel-control" href="#imgcarousel" role="button" data-slide="next">')
-    $("#imgcarousel").append('<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>')
-    $("#imgcarousel").append('<span class="sr-only">Next</span>')
-    $("#imgcarousel").append('</a>')
+    $(".carousel").carousel('pause');
 
 
     $(".imggrid").find("a").each(function() {
         $(this).click(function(event) {
             event.preventDefault();
-            alert($(this).attr("href"))
+            for(i = 0; i < numitems; i++) {
+                if(imglist[i] == $(this).attr("href"))
+                {
+                    number = i;
+                    break;
+                }
+            }
+            $(".carousel").carousel(number);
+            $("#carouselbg").fadeIn();
         });
     });
 });
+
+console.log(imglist)
