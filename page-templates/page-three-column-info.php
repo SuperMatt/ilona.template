8ec12d34d231660c8702df9d50b16dfafe4e7d0d
Matthew Ames <git@matty.digital>
2016-03-20 17:51:00 +0000

    fixed responsiveness, added three col info page, moved header and footer functions

---
+++ page-templates/page-three-column-info.php
@@ -0,0 +1,40 @@
+<?php /* Template Name: Three Column with Info */ ?>
+
+
+<?php
+    get_header();
+?>
+      <div class="row main">
+      <div class="col-sm-4 imggrid">
+        <?php the_field('col1'); ?>
+      </div>
+      <div class="col-sm-4 imggrid">
+        <?php the_field('col2'); ?>
+      </div>
+      <div class="col-sm-4 imggrid">
+        <?php the_field('col3'); ?>
+      </div>
+      </div>
+      <div class="row main">
+      <div class="col-sm-4">
+        <?php the_field('info1'); ?>
+      </div>
+      <div class="col-sm-4">
+        <?php the_field('info2'); ?>
+      </div>
+      <div class="col-sm-4">
+        <?php the_field('info3'); ?>
+      </div>
+<?php
+    if ( have_posts() ) : while ( have_posts() ) : the_post();
+        the_content();
+    endwhile;
+    else:
+        _e('Sorry, no posts matched your criteria.');
+    endif;
+?>
+      </div>
+<?php
+    get_footer();
+?>
+
