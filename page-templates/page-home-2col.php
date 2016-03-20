7d2a30eb2b4f9c3179332747390d580831973ead
Matthew Ames <matthew@tim.home>
2016-03-20 21:57:06 +0000

    added 2 hol home page

---
+++ page-templates/page-home-2col.php
@@ -0,0 +1,28 @@
+<?php /* Template Name: Home - Two Column */ ?>
+
+
+<?php
+    wp_head();
+    get_header();
+?>
+      <div class="row main">
+      <div class="col-sm-8 imggrid">
+        <?php the_field('col1'); ?>
+      </div>
+      <div class="col-sm-4 imggrid">
+        <?php the_field('col2'); ?>
+      </div>
+<?php
+    if ( have_posts() ) : while ( have_posts() ) : the_post();
+        the_content();
+    endwhile;
+    else:
+        _e('Sorry, no posts matched your criteria.');
+    endif;
+?>
+      </div>
+<?php
+    get_footer();
+    wp_footer();
+?>
+
