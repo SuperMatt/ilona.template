ffb630f4bd50076e6b352c494af9d4e48169477d
Matthew Ames <git@matty.digital>
2016-04-24 15:20:18 +0100

    added new front page

---
+++ page-templates/home-2row.php
@@ -0,0 +1,43 @@
+<?php /* Template Name: Home - Two Row */ ?>
+
+
+<?php
+    wp_head();
+    get_header();
+?>
+      <div class="row main">
+      <div class="col-sm-8 imggrid">
+        <?php the_field('col1'); ?>
+      </div>
+      <div class="col-sm-4 imggrid">
+        <?php the_field('col2'); ?>
+      </div>
+      </div>
+      <divclass="row main">
+      <div class="col-sm-4">
+        <?php the_field('row2-col1');
+      </div>
+      <div class="col-sm-4">
+        <?php the_field('row2-col2');
+      </div>
+      <div class="col-sm-4">
+        <?php the_field('row2-col3');
+      </div>
+      </div>
+
+      <div class="row">
+
+<?php
+    if ( have_posts() ) : while ( have_posts() ) : the_post();
+        the_content();
+    endwhile;
+    else:
+        _e('Sorry, no posts matched your criteria.');
+    endif;
+?>
+      </div>
+<?php
+    get_footer();
+    wp_footer();
+?>
+
