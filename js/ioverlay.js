0a15d4571cde00ae3a6fab295c788ff6f92bd4df
Matthew Ames <git@matty.digital>
2016-04-26 19:06:21 +0100

    trying to fix by replacing p with its own content

--- js/ioverlay.js
+++ js/ioverlay.js
@@ -7,6 +7,10 @@ $(document).ready(function() {
         }
     });
 
+    $(".imggrid").each( function () {
+        pContent = $(this).find('p').html();
+        $(this).html(pContent);
+    });
 
 
     $( ".wp-caption" ).each(function() {
