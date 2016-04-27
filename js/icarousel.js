3f839845e717098e35650e87d5d038ea9a4a665f
Matthew Ames <git@matty.digital>
2016-04-27 13:10:02 +0100

    added caption logging

--- js/icarousel.js
+++ js/icarousel.js
@@ -32,6 +32,7 @@ $(document).ready(function() {
         $(".carousel-inner").append('<div id="item' + i + '" class="item ' + active + '"></div>')
         captionHTML = "";
         if (captionlist[i] != "") {
+            console.log(captionlist[i])
             captionHTML = '<p class="icarousel-caption">' + captionlist[i] + '</p>'
         }
         $("#item" + i).append('<center><img src="' + imglist[i] + '" class="icarousel-img" >' + captionHTML + '</center>')
