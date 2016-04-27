8ccc28a473f1adba2b626be9087bc6d7a7884ec3
Matthew Ames <git@matty.digital>
2016-04-27 13:51:18 +0100

    added some logging

--- js/ilightbox.js
+++ js/ilightbox.js
@@ -5,6 +5,7 @@
     $( ".wp-caption" ).each(function() {
         caption = $( this ).find('img').attr('alt');
         $(this).find('a').each(function() {
+            console.log('banana');
             $(this).attr('data-lightbox', 'icarousel');
             $(this).attr('data-title', caption);
         });
