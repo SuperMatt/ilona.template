0797069309ec7a88285cd49ff89b87b44cbf012d
Ilona Porter <ilona@ilona.graphics>
2016-06-12 12:10:43 +0100

    added svg upload

--- functions.php
+++ functions.php
@@ -13,4 +13,11 @@ if ( ! isset( $content_width ) ) {
 
 add_action( 'init', 'register_my_menus');
 add_image_size('3col');
+
+function cc_mime_types($mimes) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
+
 ?>
